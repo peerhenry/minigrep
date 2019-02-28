72727cbd84648fd0684b47e1866914d14059f3ac
PeerHenry <peer@infi.nl>
2019-02-28 11:11:41 +0100

    run method in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,31 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
 pub struct Config {
   pub query: String,
   pub filename: String
 }
 
 impl Config {
-  pub fn new(args: Vec<String>) -> Self {
-    Config {
-      query: args[1].clone(),
-      filename: args[2].clone()
+  pub fn new(args: Vec<String>) -> Result<Self, &'static str> {
+    if args.len() < 3 {
+      return Err("not enough parameters");
     }
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Ok(Config {query, filename })
   }
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+  read_file(&config.filename)
+}
+
+fn read_file(file_path: &str) -> Result<(), Box<Error>> {
+  let mut f = File::open(file_path)?;
+  let mut contents = String::new();
+  f.read_to_string(&mut contents)?;
+  println!("File content:\n{}", contents);
+  Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
 use minigrep::Config;
+use minigrep::run;
+use std::process; // without this error is unwrap_or_else return type
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  if args.len() < 3 {
-    panic!("incorrect");
-  }
-  let config: Config = Config::new(args);
+  let config: Config = Config::new(args).unwrap_or_else(|err| {
+    println!("Problem parsing arguments: {}", err);
+    process::exit(1); // why not panic?
+  });
   println!("Searching for {}", config.query);
   println!("In file {}", config.filename);
-  read_file(&config.filename);
-}
-
-fn read_file(file_path: &str) {
-  let mut f = File::open(file_path).expect("file not found");
-  let mut contents = String::new();
-  f.read_to_string(&mut contents).expect("Something went wrong while reading the file");
-  println!("File content:\n{}", contents);
+  if let Err(e) = run(config) {
+    println!("Application error: {}", e);
+    process::exit(1);
+  }
 }
